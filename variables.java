fceff743523598a69b8a8007431905b81a8c0b07
kalyan-maker <kalyanraju531@gmail.com>
2026-06-05 17:56:40 +0530

    varaibles in java

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[] args) {
+        int a = 10;
+
+        String b = "kalyan";
+
+        char c = 'D';
+
+        System.out.println(c);
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
