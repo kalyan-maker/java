b237f1eb630c53961228065a6a4c42f7dae72163
kalyan-maker <kalyanraju531@gmail.com>
2026-06-09 16:01:13 +0530

    by using scanner input method to print an integer value

---
+++ sumofnumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sumofnumbers {
+     public static void main(String[] args){
+        int kalyan; // to print the integer value
+
+        Scanner ab = new Scanner(System.in); // scanner is used to take input
+        kalyan =  ab.nextInt(); // nextint it takes integer value
+        System.out.println(kalyan); // it print the vriable.
+
+        ab.close();
+
+
+     }
+}
